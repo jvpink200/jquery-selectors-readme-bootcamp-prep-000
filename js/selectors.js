d95e1c3adec6fb96ab9bc4554baa5a22cf30a3b9
jvpink200 <jvillarr20@gmail.com>
2018-08-21 22:27:54 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,16 @@
 // declare your functions here...
+function paragraphSelector(){
+  var p = $("p");
+}
+
+paragraphSelector();
+
+function lastImageSelector(){
+
+}
+
+function ninjaBabySelector(){
+  var ninja = $("img[id='baby-ninja']");
+  return ninja;
+}
+
